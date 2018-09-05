ad8715a59554f92c91abe8bf23c002556cfdb376
962457197 <962457197@qq.com>
2018-09-06 00:32:00 +0800

    no message

--- myWife/webstorm/routes/OpenMusic.js
+++ myWife/webstorm/routes/OpenMusic.js
@@ -1,34 +1,8 @@
-// var http = require('http');
-// //设置请求地址
-// var baiduUrl={
-//     "getUrl":"https://dev-openapi.dmhmusic.com/OPENAPI/openApiLogin.json"
-// }
-// //设置基本参数
-// var Basics={
-//     q_source:"lIdKAFMQxgNJM",
-//     zone:"CN"
-// }
-//
-// var a = function(){
-//     var results=http.postUrl(baiduUrl.getUrl,Basics);
-//     console.log("=========="+results);
-// }
-// a();
-
-
-
-
-
-
-
+var NodeRSA = require('node-rsa');
 var request = require('request');
+var express = require('express');
 var url="https://dev-openapi.dmhmusic.com/OPENAPI/openApiLogin.json";
-var requestData={
-    q_source: "lIdKAFMQxgNJM",
-    zone: "CN"
-}
-
-httprequest(url,requestData);
+httprequest(url);
 
 
 function httprequest(url,requestData){
@@ -38,12 +12,61 @@ function httprequest(url,requestData){
         method: "POST",
         json: true,
         headers: {
-            "content-type": "application/json",
+            'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: requestData
+        form: { q_source: 'lIdKAFMQxgNJM', zone: 'cn' }
     }, function(error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log(body) // 请求成功的处理逻辑
+           // console.log(body) // 请求成功的处理逻辑
+            var publickey=body.data;
+            //console.log("==========="+publickey);
+            getother(publickey)
+
+
         }
     });
 };
+//处理publickey
+function  getother(publickey){
+   var a=publickey.replace("-----BEGIN PUBLIC KEY-----","");
+   var key=a.replace("-----END PUBLIC KEY-----","");
+    console.log("==========="+key);
+    getservice(key);
+}
+
+function httprequest2(url,data){
+    request({
+        url: url+"&"+data,
+        gzip:true,
+        method: "POST",
+        json: true,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        // form: { q_source: 'lIdKAFMQxgNJM', zone: 'cn' }
+    }, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body) // 请求成功的处理逻辑
+            var publickey=body.data;
+            //console.log("==========="+publickey);
+            getother(publickey)
+
+
+        }
+    });
+};
+//调用其它接口
+function getservice(key){
+    var urlservice='https://dev-openapi.dmhmusic.com/auth/';
+    var param={"action":"/OPENAPI/getSpSessionBizList.json","method":"POST"};
+    var rsa = RSA(param,key);
+
+    httprequest2(urlservice,rsa);
+}
+//RSA加密
+function RSA(json,key){
+    key = new NodeRSA(key,'pkcs8-public');
+    var encrypted = key.encrypt(json, 'base64');
+    console.log("==========="+encrypted);
+    return encrypted;  // 加密后数据
+}
